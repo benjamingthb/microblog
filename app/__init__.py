9b6c4020e8bc8e9045bc2692d847bd29b6e05cfa
benjamin gthb <lmcheng@foxmail.com>
2020-03-26 21:25:08 +0800

    second commit

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap #Twitter推出的CSS框架
 
 app_flsk = Flask(__name__)
 app_flsk.config.from_object(Config)
@@ -10,5 +11,6 @@ db = SQLAlchemy(app_flsk)
 migrate = Migrate(app_flsk, db)
 login = LoginManager(app_flsk)
 login.login_view = 'login'
+bootstrap = Bootstrap(app_flsk)
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -48,10 +48,28 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app_flsk.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form,
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
+
+    '''
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app_flsk.config['POSTS_PER_PAGE'], False)
+    return render_template('index.html', title='Home', form=form,
+                           posts=posts.items)
+
     posts = current_user.followed_posts().all()
     return render_template("index.html", title='Home Page', form=form,
                            posts=posts)
-
+    '''
 
 '''
 @app_flsk.route('/login', methods=['GET', 'POST'])
@@ -108,6 +126,7 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+'''
 @app_flsk.route('/user/<username>')
 @login_required
 def user(username):
@@ -117,6 +136,23 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+'''
+
+@app_flsk.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app_flsk.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
+
+
 
 @app_flsk.before_request
 def before_request():
@@ -171,8 +207,31 @@ def unfollow(username):
     flash('You are not following {}.'.format(username))
     return redirect(url_for('user', username=username))
 
+'''
 @app_flsk.route('/explore')
 @login_required
 def explore():
     posts = Post.query.order_by(Post.timestamp.desc()).all()
     return render_template('index.html', title='Explore', posts=posts)
+
+@app_flsk.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app_flsk.config['POSTS_PER_PAGE'], False)
+    return render_template("index.html", title='Explore', posts=posts.items)
+'''
+
+@app_flsk.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app_flsk.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template("index.html", title='Explore', posts=posts.items,
+                          next_url=next_url, prev_url=prev_url)
